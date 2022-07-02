a8da5503eac1568f06eb9b987da74836dcc39ad3
Tribe-Wolf17 <108558895+Tribe-Wolf17@users.noreply.github.com>
2022-07-01 21:49:28 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+function setup() {
+  createCanvas(400,400);
+  var ball = createSprite(199,201,50,50)
+ball.shapeColor="white"
+  
+}
+
+function draw() 
+{
+  background(170,50,170);
+drawSprites()
+}
+
+
+
+
